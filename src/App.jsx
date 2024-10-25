3392bcd9fae957f28e0f4c65702b38d9e85b8736
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-25 21:53:32 +0330

    chore: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./components/Authentication/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import DashboardRoutes from "./Routes/DashboardRoutes";
 
 const App = () => {
-  return <div>App</div>;
+  const { user } = useAuth();
+
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              user ? (
+                <Navigate to="/dashboard" replace />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+
+          {/* Protected Route for Dashboard */}
+
+          <Route
+            path="/dashboard/*"
+            element={
+              user ? <DashboardRoutes /> : <Navigate to="/login" replace />
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 };
 
 export default App;
