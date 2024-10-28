ac93b8c7a8b08b6aa0436a9c77d14ce9f924607d
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-28 19:20:19 +0330

    fix: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,45 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { AuthProvider, useAuth } from "./Components/Authentication/AuthContext";
+// import { AuthProvider, useAuth } from "./Components/Authentication/AuthContext";
 import LoginPage from "./pages/LoginPage";
 import ForgetPasswordPage from "./pages/ForgetPasswordPage";
 import DashboardRoutes from "./Routes/DashboardRoutes";
 
 const App = () => {
-  const { user } = useAuth();
+  // const { user } = useAuth();
 
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              user ? (
-                <Navigate to="/dashboard" replace />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+    // <AuthProvider>
+    //   <Router>
+    //     <Routes>
+    //       <Route
+    //         path="/"
+    //         element={
+    //           user ? (
+    //             <Navigate to="/dashboard" replace />
+    //           ) : (
+    //             <Navigate to="/login" replace />
+    //           )
+    //         }
+    //       />
+    //       <Route path="/login" element={<LoginPage />} />
+    //       <Route path="/forget-password" element={<ForgetPasswordPage />} />
 
-          {/* Protected Route for Dashboard */}
+    //       {/* Protected Route for Dashboard */}
 
-          <Route
-            path="/dashboard/*"
-            element={
-              user ? <DashboardRoutes /> : <Navigate to="/login" replace />
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
-    // <div className="">
-    //   <LoginPage />
-    // </div>
+    //       <Route
+    //         path="/dashboard/*"
+    //         element={
+    //           user ? <DashboardRoutes /> : <Navigate to="/login" replace />
+    //         }
+    //       />
+    //     </Routes>
+    //   </Router>
+    // </AuthProvider>
+    <>
+      <LoginPage />
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <div>hi</div>
+    <App />
   </StrictMode>
 );
