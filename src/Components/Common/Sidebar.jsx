c18d0ccc20af5d2868bc52c057c9c3950ffc6062
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-28 21:12:30 +0330

    feat: Add Sidebar

--- src/Components/Common/Sidebar.jsx
+++ src/Components/Common/Sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="h-screen w-24 bg-customBlue flex flex-col justify-between items-center py-4">
+      <div className="flex flex-col space-y-10 items-center">
+        <img src="/src/Assets/Icons/AdklayLogo.svg" alt="" className="mb-4" />
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/icon.svg"
+          label="متن تست"
+        />
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/icon.svg"
+          label="متن تست"
+        />
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/icon.svg"
+          label="متن تست"
+        />
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/icon.svg"
+          label="متن تست"
+        />
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/icon.svg"
+          label="متن تست"
+        />
+      </div>
+      <div className="mb-4">
+        <SidebarItem
+          to="/"
+          icon="/src/Assets/Icons/Sidbar/log-out.svg"
+          label="خروج"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
