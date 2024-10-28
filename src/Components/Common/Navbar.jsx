6b0b77b406bef9d9895ab62e776c09ef89be37d1
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-28 21:12:49 +0330

    feat: Add Navbar

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom"; // useNavigate را وارد کنید
+import { FaBell, FaUser } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="w-full h-24 flex items-center justify-between px-14">
+      <div className="flex items-center space-x-4">
+        <Link
+          to="/"
+          className="w-11 h-11 bg-customBlue rounded-full flex items-center justify-center text-white hover:bg-blue-800 ml-3"
+        >
+          <FaUser />
+        </Link>
+        <Link
+          to="/"
+          className="w-11 h-11 bg-customBlue rounded-full flex items-center justify-center text-white hover:bg-blue-800"
+        >
+          <FaBell />
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={handleBack}
+          className="w-11 h-11 bg-customBlue rounded-full flex items-center justify-center text-white hover:bg-blue-800"
+        >
+          <IoIosArrowBack />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
