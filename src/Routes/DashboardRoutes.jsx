1c2dd1843deedb9dde42fa9c8d1678177b43f1d0
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-29 15:10:40 +0330

    feat: ProtectedRoute.jsx

--- src/Routes/DashboardRoutes.jsx
+++ src/Routes/DashboardRoutes.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "./../Components/Layout/Layout";
 import ContractListPage from "../Pages/ContractListPage";
+import AddContractPage from "../Pages/AddContractPage";
 
 const DashboardRoutes = () => {
   return (
     <Layout>
+      {/* <Routes> */}
       <ContractListPage />
+      {/* </Routes> */}
     </Layout>
   );
 };
