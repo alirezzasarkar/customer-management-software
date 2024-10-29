c8759693ce5fb866c36c5bb3880bf0bbe99f11b3
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-29 11:01:17 +0330

    fix: Sidebar.jsx

--- src/Components/Common/Sidebar.jsx
+++ src/Components/Common/Sidebar.jsx
@@ -3,42 +3,48 @@ import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-24 bg-customBlue flex flex-col justify-between items-center py-4">
-      <div className="flex flex-col space-y-10 items-center">
-        <img src="/src/Assets/Icons/AdklayLogo.svg" alt="" className="mb-4" />
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/icon.svg"
-          label="متن تست"
-        />
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/icon.svg"
-          label="متن تست"
-        />
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/icon.svg"
-          label="متن تست"
-        />
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/icon.svg"
-          label="متن تست"
-        />
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/icon.svg"
-          label="متن تست"
-        />
-      </div>
-      <div className="mb-4">
-        <SidebarItem
-          to="/"
-          icon="/src/Assets/Icons/Sidbar/log-out.svg"
-          label="خروج"
-        />
-      </div>
+    <div className="h-auto w-24 bg-customBlue flex flex-col space-y-10 justify-between items-center py-4">
+      <img src="/src/Assets/Icons/AdklayLogo.svg" alt="" className="mb-4" />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/icon.svg"
+        label="متن تست"
+      />
+      <SidebarItem
+        to="/"
+        icon="/src/Assets/Icons/Sidbar/log-out.svg"
+        label="خروج"
+      />
     </div>
   );
 };
