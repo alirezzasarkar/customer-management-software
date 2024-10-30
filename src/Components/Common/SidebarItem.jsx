f1876ca19424652b82cba307fbf994484a197a67
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-10-30 11:38:57 +0330

    feat: Sidbar.jsx

--- src/Components/Common/SidebarItem.jsx
+++ src/Components/Common/SidebarItem.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const SidebarItem = ({ to, icon, label }) => {
-  const [hovered, setHovered] = useState(false);
+const SidebarItem = ({ to, icon, label, subItems }) => {
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Link
-      to={to}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      className="text-white w-8 h-8 rounded-full bg-customBlue hover:bg-blue-800 flex items-center justify-center relative"
+    <div
+      className="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <img src={icon} alt={label} />
-      {hovered && (
-        <span className="absolute right-12 bg-gray-800 text-white whitespace-nowrap text-xs rounded-md py-1 px-5">
-          {label}
-        </span>
+      <Link to={to} className="flex flex-col items-center">
+        <img src={icon} alt={label} className="h-6 w-6 mb-2" />
+        <span className="text-xs text-white">{label}</span>
+      </Link>
+
+      {isHovered && subItems && (
+        <div className="absolute right-full top-0 mt-2 w-max bg-white/70 backdrop-blur-lg shadow-lg rounded-lg border border-gray-200">
+          {subItems.map((subItem, index) => (
+            <Link
+              key={index}
+              to={subItem.to}
+              className="block text-xs px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md"
+            >
+              {subItem.label}
+            </Link>
+          ))}
+        </div>
       )}
-    </Link>
+    </div>
   );
 };
 
