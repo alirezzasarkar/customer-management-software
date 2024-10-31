722f752017ceac458ca2ccf15357577c2617acb8
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2024-10-31 14:00:04 +0330

    style:Edit button removed from sales opportunity page, search input applied to dropdowns,hover state added to submit button

--- src/Components/Common/DashboardDropDown.jsx
+++ src/Components/Common/DashboardDropDown.jsx
@@ -42,11 +42,22 @@ export default function DashboardDropDown({ label_text }) {
       </button>
 
       {isOpen && (
-        <ul className="absolute top-1/2 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none">
-          <div className="py-1"></div>
-          <li>ss</li>
-          <li>ss</li>
-          <li>ss</li>
+        <ul className="absolute top-1/2 left-0 z-10 mt-3 w-56 origin-top-right rounded-lg bg-white ring-opacity-5 transition focus:outline-none p-2 text-center text-sm space-y-3 text-[#0E295B]">
+          <div className="relative mb-2">
+            <input
+              type="search"
+              placeholder="جستجو"
+              className="placeholder:text-[#0E295B] placeholder:text-xs placeholder:font-bold w-full px-3 py-2 border-2 border-[#153D8A] rounded-lg focus:outline-none"
+            />
+            <img
+              src="/src/Assets/Icons/Search-icon.svg"
+              alt="Search Icon"
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 "
+            />
+          </div>
+          <li className="border-b">متن تست</li>
+          <li className="border-b">متن تست</li>
+          <li>متن تست</li>
         </ul>
       )}
     </div>
