fad09b703affc9f3572af36ffa5e83bd9a02c07d
Alireza Sarkar <alirezasarkar20@gmail.com>
2024-11-06 14:36:53 +0330

    feat: SalesOpportunitiesDetail.jsx

--- src/Components/SalesOpportunities/SalesOpportunitiesDetail.jsx
+++ src/Components/SalesOpportunities/SalesOpportunitiesDetail.jsx
@@ -1,9 +1,61 @@
-
+import Title from "../Common/Title";
+import DashboardButton from './../Common/DashboardButton';
 
 const SalesOpportunitiesDetail = () => {
   return (
-    <div>SalesOpportunitiesDetail</div>
-  )
-}
+    <>
+      <Title title="جزئیات فرصت فروش" />
+      <div className="bg-gray-100 p-10 mx-6 rounded-md">
+        <div className="grid grid-cols-3 gap-4 mb-4">
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">نام و نام خانوادگی</span>
+            <p className="text-gray-700 mt-2">لیلا کردی</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">تاریخ پیگیری</span>
+            <p className="text-gray-700 mt-2">۱۳۸۱/۰۴/۱۱</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">اولویت فرصت</span>
+            <p className="text-gray-700 mt-2">متوسط</p>
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-3 gap-4 mt-10 mb-4">
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">محصول مورد نظر</span>
+            <p className="text-gray-700 mt-2">درب ضد سرقت</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">مبلغ تخمینی</span>
+            <p className="text-gray-700 mt-2">۳۰۰,۰۰۰,۰۰۰ تومان</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-blue-800 font-semibold">افزودن یادداشت</span>
+            <p className="text-gray-700 mt-2">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است.
+            </p>
+          </div>
+        </div>
 
-export default SalesOpportunitiesDetail
+        <div className="flex justify-center gap-5 mt-10">
+            <DashboardButton
+              inner_text="ویرایش اطلاعات"
+              icon="/src/Assets/Icons/edit.svg"
+              bg_color="bg-[#FF6500]"
+              hover_state="hover:bg-[#FF6500]"
+            />
+            <DashboardButton
+              inner_text="حذف اطلاعات"
+              icon="/src/Assets/Icons/delete.svg"
+              bg_color="bg-[#FF0000]"
+              hover_state="hover:bg-[#FF0000]"
+            />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SalesOpportunitiesDetail;
