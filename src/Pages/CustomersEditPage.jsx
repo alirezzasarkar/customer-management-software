5ff7d81f03838f623142c3d77df19f3498125e53
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2024-11-10 14:37:28 +0330

    feat:Customer edit page added

---
+++ src/Pages/CustomersEditPage.jsx
@@ -0,0 +1,11 @@
+import CustomersEdit from "../Components/Customers/CustomersEdit";
+
+const CustomersEditPage = () => {
+  return (
+    <div>
+      <CustomersEdit />
+    </div>
+  );
+};
+
+export default CustomersEditPage;
