030a04104b5108b76c3b6f21e3a1be5bb96372cf
Amirhosein Fallahan <124490433+PartialTruss@users.noreply.github.com>
2024-11-30 09:34:36 +0330

    style:Responsive design applied to detail page

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Sidebar from "../Common/Sidebar";
+import React from "react";
 import Navbar from "../Common/Navbar";
+import Sidebar from "../Common/Sidebar";
 
 const Layout = ({ children }) => {
   return (
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Navbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-        <main className="flex-1 p-6 text-right overflow-y-auto">
+        <main className="flex-1 sm:p-6 p-5 text-right overflow-y-auto">
           {children}
         </main>
       </div>
--- src/Components/Marketing/CampaignDetail.jsx
+++ src/Components/Marketing/CampaignDetail.jsx
@@ -1,50 +1,63 @@
 import Title from "../Common/Title";
-import DashboardButton from './../Common/DashboardButton';
+import DashboardButton from "./../Common/DashboardButton";
 
 const CampaignDetail = () => {
   return (
     <>
       <Title title="جزئیات کمپین" />
-      <div className="bg-gray-100 p-10 mx-6 rounded-md">
-        <div className="grid grid-cols-3 gap-4 mb-4">
+      <div className="bg-gray-100 sm:p-10 p-5 sm:mx-6 rounded-md">
+        <div className="grid sm:grid-cols-3 grid-cols-1 gap-4 mb-4">
           <div className="flex flex-col">
-            <span className="text-sm text-blue-800 font-semibold">نام کمپین</span>
+            <span className="text-sm text-blue-800 font-semibold">
+              نام کمپین
+            </span>
             <p className="text-gray-700 mt-2">کمپین پاییزه</p>
           </div>
           <div className="flex flex-col">
-            <span className="text-sm text-blue-800 font-semibold">شروع کمپین</span>
+            <span className="text-sm text-blue-800 font-semibold">
+              شروع کمپین
+            </span>
             <p className="text-gray-700 mt-2">۱۳۸۱/۰۴/۱۱</p>
           </div>
           <div className="flex flex-col">
-            <span className="text-sm text-blue-800 font-semibold">پایان کمپین</span>
+            <span className="text-sm text-blue-800 font-semibold">
+              پایان کمپین
+            </span>
             <p className="text-gray-700 mt-2">۱۳۸۱/۰۴/۱۱</p>
           </div>
         </div>
-        
-        <div className="grid grid-cols-3 gap-4 mt-10 mb-4">
+
+        <div className="grid sm:grid-cols-3 grid-cols-1 gap-4 mt-10 mb-4">
           <div className="flex flex-col">
-            <span className="text-sm text-blue-800 font-semibold">مخاطبین هدف</span>
+            <span className="text-sm text-blue-800 font-semibold">
+              مخاطبین هدف
+            </span>
             <p className="text-gray-700 mt-2">سجاد باقریان، علیرضا سرکار</p>
           </div>
           <div className="flex flex-col">
-            <span className="text-sm text-blue-800 font-semibold">متن پیام</span>
-            <p className="text-gray-700 mt-2">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ  و با استفاده از طراحان گرافیک است</p>
+            <span className="text-sm text-blue-800 font-semibold">
+              متن پیام
+            </span>
+            <p className="text-gray-700 mt-2">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است
+            </p>
           </div>
         </div>
 
         <div className="flex justify-center gap-5 mt-20">
-            <DashboardButton
-              inner_text="ویرایش اطلاعات"
-              icon="/src/Assets/Icons/edit.svg"
-              bg_color="bg-[#FF6500]"
-              hover_state="hover:bg-[#FF6500]"
-            />
-            <DashboardButton
-              inner_text="حذف اطلاعات"
-              icon="/src/Assets/Icons/delete.svg"
-              bg_color="bg-[#FF0000]"
-              hover_state="hover:bg-[#FF0000]"
-            />
+          <DashboardButton
+            inner_text="ویرایش اطلاعات"
+            icon="/src/Assets/Icons/edit.svg"
+            bg_color="bg-[#FF6500]"
+            hover_state="hover:bg-[#FF6500]"
+          />
+          <DashboardButton
+            inner_text="حذف اطلاعات"
+            icon="/src/Assets/Icons/delete.svg"
+            bg_color="bg-[#FF0000]"
+            hover_state="hover:bg-[#FF0000]"
+          />
         </div>
       </div>
     </>
